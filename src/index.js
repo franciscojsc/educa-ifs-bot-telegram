310e8df167a5c4c16cb2f0ed61b6c3cb3f5cee56
Francisco Chaves <francisco.jschaves@gmail.com>
2021-04-13 11:53:43 -0300

    feat: Use the buttons for the main menu

--- src/index.js
+++ src/index.js
@@ -5,6 +5,10 @@ const bot = new Telegraf(env.token);
 const path = require('path');
 const botImage = path.join(__dirname, 'assets', 'imgs', 'educaIFS_bot.png');
 
+const buttonsKeyboard = require('./buttons/buttonsKeyboard');
+const buttonStart = buttonsKeyboard.oneButton('Vamos começar?');
+const buttonMenuDefault = buttonsKeyboard.twoButton('Quiz', 'Tutoriais', 2);
+
 bot.catch((err, ctx) => {
   ctx.reply(
     `Estou com algum problema hoje, poderia conversar comigo mais tarde 🤕.`
@@ -16,7 +20,17 @@ bot.start(async (ctx) => {
   await ctx.reply(
     `Olá ${nome}, eu sou um Bot 🤖 e o meu nome é Educa IFS, mas você pode me chamar de Edu 😃!`
   );
-  await ctx.replyWithPhoto({ source: botImage });
+  await ctx.replyWithPhoto({ source: botImage }, buttonStart);
+});
+
+bot.hears([/vamos começar/i], async (ctx) => {
+  await ctx.reply(
+    'Meus conhecimentos atualmente são sobre a tecnologia Git, se tiver alguma dúvida, envie uma mensagem para mim, e tentarei responder, 😉 Ok'
+  );
+  await ctx.reply(
+    'Clique nos botões abaixo, se quiser entrar no Quiz e testar seus conhecimentos, ou seguir os tutoriais disponíveis, vai ser bem legal!!!',
+    buttonMenuDefault
+  );
 });
 
 bot.launch();
