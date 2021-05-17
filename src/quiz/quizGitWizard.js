abfe2d0fd103e266a143e7878b8ae256e1aaca46
Francisco Chaves <francisco.jschaves@gmail.com>
2021-05-17 09:39:09 -0300

    feat: Add step to Quiz

--- src/quiz/quizGitWizard.js
+++ src/quiz/quizGitWizard.js
@@ -1,12 +1,18 @@
 const WizardScene = require('telegraf/scenes/wizard');
 
+const questionGitComposer = require('./step/questionGitComposer');
+
+const buttonsInline = require('./../buttons/buttonsInline');
+const buttonOK = buttonsInline.oneButton('Vamos lá?', 'ok');
 const id = 'QUIZ_GIT';
 
 const quizGitWizard = new WizardScene(
   id,
   async (ctx) => {
+    ctx.reply('😎', buttonOK);
     return ctx.wizard.next();
   },
+  questionGitComposer,
   (ctx) => {
     ctx.reply('Tchau');
     return ctx.scene.leave();
